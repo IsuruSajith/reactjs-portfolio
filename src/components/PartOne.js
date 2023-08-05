f56ef5ef2de7f0e72963654ff6a2843788a02b88
Isuru <isurusajith0@gmail.com>
2023-08-05 07:07:06 +0530

    parsed children props

--- src/components/PartOne.js
+++ src/components/PartOne.js
@@ -3,17 +3,16 @@ import './partone.css'
 
 export default function PartOne(props) {
 
-const text = "this is dynamic text in component part one"
+    const text = "this is dynamic text in component part one"
 
 
-  return (
-    <div className='part-one'>
-      <div className='box'>
-        {text}
-        <br/>
-        {"my name is : "+props.name +"\n and my occupation is :"+props.occupation}
-
-      </div>
-  </div>
-  )
+    return (<div className='part-one'>
+            <div className='box'>
+                {text}
+                <br/>
+                {"my name is : " + props.name + "\n and my occupation is :" + props.occupation}
+                <br/>
+                {"this is my children props: " + props.children}
+            </div>
+        </div>)
 }
