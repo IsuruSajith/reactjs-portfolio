d1d47bc9668556aa61b6e180d67583d07393ecf7
Isuru <isurusajith0@gmail.com>
2023-08-05 10:17:39 +0530

    added routing..
    got some few errors using switch element, nowadays it used route for switch, it is important that element heirachy to remember

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 
 function App() {
-  return (
-    <div className="App">
-     <Home/>
-    </div>
-    
-  );
+    return (
+    <Router>
+            <div>
+                <Routes>
+                  <Route exact path='/' element={<Home/>}/>
+              
+                  <Route path='/about' element={<About/>}/>
+            
+                  <Route path='/contact' element={<Contact/>}/>
+              
+                </Routes>
+            </div>
+
+        </Router>
+
+    );
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,14 @@ import React from 'react'
 import PartOne from '../components/PartOne'
 
 
-export default function HOme() {
+export default function Home() {
   return (
     <div>
         
         <h1>this is my first page in home</h1>
-        <PartOne name="isuru" occupation="software engineer">
-            this is children props
-        </PartOne>
-        <PartOne name="sajith" occupation="senior software engineer">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor dolore ipsam mollitia nobis.
-            Eveniet expedita, quo. </p>
+      <PartOne name='Isuru'>
 
-        </PartOne>
-        <PartOne name="rathnayaka" occupation="lead software engineer">
-            <button>click me</button>
-        </PartOne>
+      </PartOne>
 
 
         </div>
