3714a34d9e1433e0cd7c377d5a5907ba95451ab8
Isuru <isurusajith0@gmail.com>
2023-08-05 11:34:04 +0530

    useState Hook
    create a counter, it gives single page countering, run and see the difference ;)

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 
 export default function About() {
   return (
-    <div>This is about page</div>
+    <div>This is about page
+
+        <br />
+        <Link to='/Home'>Home</Link>
+    </div>
+
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import PartOne from '../components/PartOne'
+import {Link} from 'react-router-dom'
+import { useState } from 'react'
 
 
 export default function Home() {
+
+    const[count,setCount]=useState(0);
+
+    const increase=()=>{
+        setCount(count+1);
+    }
+    const decrease=()=>{
+
+        setCount(count-1);
+    }
+
   return (
     <div>
         
@@ -11,6 +24,15 @@ export default function Home() {
 
       </PartOne>
 
+      <Link to='/About'>About</Link>
+      <br />
+      <Link to ='/Contact'>Contact us</Link>
+
+      <hr />
+      <h3>My counter</h3>
+      <h5>My count is:  {count}</h5>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
 
         </div>
   )
