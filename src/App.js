a1e783e33d3eb011ab65d01c38a714b34e5e64d6
Isuru <isurusajith0@gmail.com>
2023-08-06 13:06:41 +0530

    make five pages and header component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-
 import './App.css';
 import Home from './pages/Home';
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 import Contact from './pages/Contact';
-import About from './pages/About';
+import Experience from "./pages/Experience";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import TechStack from "./pages/TechStack";
 
 
 function App() {
-    return (
-    <Router>
+    return (<Router>
             <div>
                 <Routes>
-                  <Route exact path='/' element={<Home/>}/>
-              
-                  <Route path='/about' element={<About/>}/>
-            
-                  <Route path='/contact' element={<Contact/>}/>
-              
+                    <Route exact path='/' element={<Home/>}/>
+                    <Route exact path='/experience' element={Experience}/>
+                    <Route path='/contact' element={<Contact/>}/>
+                    <Route path='/projects' element={<Projects/>}/>
+                    <Route path='/techstack' element={<TechStack/>}/>
+                    <Route path='/resume' element={<Resume/>}/>
                 </Routes>
             </div>
 
---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function Header() {
+  return (
+    <div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Isuru</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Experience</Nav.Link>
+            <Nav.Link href="#link">Projects</Nav.Link>
+            <Nav.Link href="#link">Education</Nav.Link>
+            <Nav.Link href="#link">Tech Stack</Nav.Link>
+            <Nav.Link href="#link">Resume</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    </div>
+  )
+}
