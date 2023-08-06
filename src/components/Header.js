39c50945d4262ef6bb4e5875663cff95549e062d
Isuru <isurusajith0@gmail.com>
2023-08-06 16:16:19 +0530

    resume download option is implemented
    use onClick option in react event handling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,15 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Header() {
+  const handleDownloadResume = () => {
+    const resumePath = 'path/to/your/resume.pdf';
+    const link = document.createElement('a');
+    link.href = resumePath;
+    link.target = '_blank';
+    link.download = 'Isuru_Sajith_Rathnayaka.pdf';
+    link.click();
+  };
+
   return (
     <div>
         <Navbar expand="lg" className="bg-body-tertiary">
@@ -18,12 +27,13 @@ export default function Header() {
             <Nav.Link href="/projects">Projects</Nav.Link>
             <Nav.Link href="/education">Education</Nav.Link>
             <Nav.Link href="/techstack">Tech Stack</Nav.Link>
-            <Nav.Link href="/resume">Resume</Nav.Link>
+            <Nav.Link href="/resume" onClick={handleDownloadResume}>Resume</Nav.Link>
             <Nav.Link href="/resume">Contact</Nav.Link>
             <NavDropdown title="Dev Toolbox" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Emoji Mood Selector</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Quote of the Day</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">BMI Calculator</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Submit your data</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Theme Switcher
