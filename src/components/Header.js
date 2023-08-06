583aa4b3176b3f2fadbc4afceb720fadec4b77ba
Isuru <isurusajith0@gmail.com>
2023-08-06 19:48:51 +0530

    set an alert for submit data option
    now going to make submit data ui and connect to back end

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,50 +6,51 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import {toast} from "react-toastify";
 
 export default function Header() {
-  const handleDownloadResume = () => {
-    const resumePath = 'path/to/your/resume.pdf';
-    const link = document.createElement('a');
-    link.href = resumePath;
-    link.target = '_blank';
-    link.download = 'Isuru_Sajith_Rathnayaka.pdf';
-    link.click();
+    const handleDownloadResume = () => {
+        const resumePath = 'path/to/your/resume.pdf';
+        const link = document.createElement('a');
+        link.href = resumePath;
+        link.target = '_blank';
+        link.download = 'Isuru_Sajith_Rathnayaka.pdf';
+        link.click();
 
-    toast.success('Resume downloaded successfully!',{
-      position:'bottom-right',
-      autoClose:3000
-    });
-  };
+        toast.success('Resume downloaded successfully!', {
+            position: 'bottom-right', autoClose: 3000
+        });
+    };
 
-  return (
-    <div>
-        <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/home">Isuru</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/experience">Experience</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/education">Education</Nav.Link>
-            <Nav.Link href="/techstack">Tech Stack</Nav.Link>
-            <Nav.Link href="/resume" onClick={handleDownloadResume}>Resume</Nav.Link>
-            <Nav.Link href="/resume">Contact</Nav.Link>
-            <NavDropdown title="Dev Toolbox" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Emoji Mood Selector</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Quote of the Day</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">BMI Calculator</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Submit your data</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Theme Switcher
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    const submitData = () => {
+alert('working');
+    }
+    return (<div>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="/home">Isuru</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="/home">Home</Nav.Link>
+                            <Nav.Link href="/experience">Experience</Nav.Link>
+                            <Nav.Link href="/projects">Projects</Nav.Link>
+                            <Nav.Link href="/education">Education</Nav.Link>
+                            <Nav.Link href="/techstack">Tech Stack</Nav.Link>
+                            <Nav.Link href="/resume" onClick={handleDownloadResume}>Resume</Nav.Link>
+                            <Nav.Link href="/resume">Contact</Nav.Link>
+                            <NavDropdown title="Dev Toolbox" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Emoji Mood Selector</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Quote of the Day</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">BMI Calculator</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2" onClick={submitData}>Submit your
+                                    data</NavDropdown.Item>
+                                <NavDropdown.Divider/>
+                                <NavDropdown.Item href="#action/3.4">
+                                    Theme Switcher
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
-    </div>
-  )
+        </div>)
 }
