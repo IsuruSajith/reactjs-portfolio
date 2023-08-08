36f438f5b6ee44c73110d8243bc98a5d19954e3d
Isuru <isurusajith0@gmail.com>
2023-08-08 10:38:34 +0530

    npm i formik yup
    for bootstrap supported dependecies, and create the forum for submit data, but css not applied for it, looking forward for the reason

--- src/components/SubmitForum.js
+++ src/components/SubmitForum.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
+import BootstrapForm from "./BootstrapForm";
 
 const openSubmitForumPopup = () => {
 
-  const popupWindow = window.open('', '_blank', 'width=400,height=300');
+    const popupWindow = window.open('', '_blank', 'width=400,height=300');
 
   if (popupWindow) {
+      const popupContainer = popupWindow.document.createElement('div');
+      popupContainer.className = 'popup-container';
+
     const popupContent = (
-        <div>
-          <h2>Submit Your Data</h2>
+        <div className="popup-container">
+            <h2 className="popup-title">Submit Your Data</h2>
+            <form className="popup-form">
+                <label>Name:</label>
+                <input type="text" name="name" />
+                <BootstrapForm/>
+            </form>
         </div>
     );
-    popupWindow.document.body.innerHTML = popupContent;
+      ReactDOM.render(popupContent, popupContainer);
+      popupWindow.document.body.appendChild(popupContainer);
   }
 };
 
