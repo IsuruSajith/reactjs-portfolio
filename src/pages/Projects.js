330884a0fa1932511a2e51812e0c6b691c614d0e
Isuru <isurusajith0@gmail.com>
2023-08-09 10:32:59 +0530

    tailwind css
    initiate , learn and try to appy my project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,43 +5,47 @@ import './css/Projects.css'
 
 
 export default function Projects() {
-  return (
-    <div className='d-flex'>
-      <Card style={{ width: '20rem' }}>
-        <Card.Img variant="top" src="/images/pos.jpg" className='image' />
-        <Card.Body>
-          <Card.Title> <h6> POS v1.0.0: Point-of-Sale System </h6></Card.Title>
-          <Card.Text className='content'>
-              <hr/>
-              A web application designed for retail shops to efficiently manage orders and daily
-              selling activities using Java, Spring Boot, and React, it offers streamlined order
-              processing, inventory control, and detailed sales reporting. The user-friendly
-              interface ensures easy navigation, while the tech stack ensures scalability and
-              optimal performance. This project showcases my expertise in developing feature-
-              rich web applications for enhanced retail operations. <br/> <hr/>
-              <b><i>[Java | MySQL | Maven | Spring boot | JUnit | Lombok | Hibernate | ECMA Script |
-              H2 | React | Nodejs]</i></b>
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="/images/pos.jpg"  />
-            <Card.Body>
-                <Card.Title>POS v1.0.0: Point-of-Sale System</Card.Title>
-                <Card.Text className='content'>
-                    A web application designed for retail shops to efficiently manage orders and daily
-                    selling activities using Java, Spring Boot, and React, it offers streamlined order
-                    processing, inventory control, and detailed sales reporting. The user-friendly
-                    interface ensures easy navigation, while the tech stack ensures scalability and
-                    optimal performance. This project showcases my expertise in developing feature-
-                    rich web applications for enhanced retail operations. <br/>
-                    [Java | MySQL | Maven | Spring boot | JUnit | Lombok | Hibernate | ECMA Script |
-                    H2 | React | Nodejs]
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
-    </div>
-  )
+    return (
+        <div className='d-flex w-100'>
+        <div className=''>
+            <Card style={{width: '18rem'}}>
+                <Card.Img variant="top" src="/images/pos.jpg" className='image'/>
+                <Card.Body>
+                    <Card.Title><h6> POS v1.0.0: Point-of-Sale System </h6></Card.Title>
+                    <Card.Text className='content'>
+
+                        A web application designed for retail shops to efficiently manage orders and daily
+                        selling activities using Java, Spring Boot, and React, it offers streamlined order
+                        processing, inventory control, and detailed sales reporting. The user-friendly
+                        interface ensures easy navigation, while the tech stack ensures scalability and
+                        optimal performance. This project showcases my expertise in developing feature-
+                        rich web applications for enhanced retail operations. <br/>
+                <br/>
+                        <b><i>[Java | MySQL | Maven | Spring boot | JUnit | Lombok | Hibernate | ECMA Script |
+                            H2 | React | Nodejs]</i></b>
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </div>
+        <div>
+            <Card style={{width: '18rem'}}>
+                <Card.Img variant="top" src="/images/pos.jpg"/>
+                <Card.Body>
+                    <Card.Title>POS v1.0.0: Point-of-Sale System</Card.Title>
+                    <Card.Text className='content'>
+                        A web application designed for retail shops to efficiently manage orders and daily
+                        selling activities using Java, Spring Boot, and React, it offers streamlined order
+                        processing, inventory control, and detailed sales reporting. The user-friendly
+                        interface ensures easy navigation, while the tech stack ensures scalability and
+                        optimal performance. This project showcases my expertise in developing feature-
+                        rich web applications for enhanced retail operations. <br/>
+                        [Java | MySQL | Maven | Spring boot | JUnit | Lombok | Hibernate | ECMA Script |
+                        H2 | React | Nodejs]
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </div>
+    </div>)
 }
