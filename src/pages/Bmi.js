43c683a403cb3e17dbdf777efbc356439b49bdcc
Isuru <isurusajith0@gmail.com>
2023-08-10 15:46:24 +0530

    BMI calc
    completed BMI calculator without validation.

--- src/pages/Bmi.js
+++ src/pages/Bmi.js
@@ -1,13 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Header from "../components/Header";
 import './css/Bmi.css'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 
 export default function Bmi() {
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+    const [bmi, setBMI] = useState("");
 
-  return (
-    <div className='bmi-container'>
-fjfdgfjjj
+    const calculateBMI = () => {
+        if (height && weight) {
+            const heightInMeters = height / 100;
+            const bmiValue = weight / (heightInMeters * heightInMeters);
+            setBMI(bmiValue.toFixed(2));
+        }
+    };
+    const renderTooltip = (props) => (<Tooltip id="button-tooltip" {...props}>
+            Compute BMI
+        </Tooltip>);
 
-    </div>
-  )
+    return (<div className='bmi-container'>
+        <div className='calculator d-flex flex-column justify-content-center align-items-center'>
+            <h2 className='text-blue-800 underline'>BMI Calculator</h2>
+            <div className='border border-gray-300 rounded-lg p-4'>
+                <Tabs
+                    defaultActiveKey="home"
+                    transition={false}
+                    id="noanim-tab-example"
+                    className="mb-3"
+                >
+                    <Tab eventKey="home" title="Metric Units">
+                        <>
+                            <FloatingLabel
+                                controlId="floatingInput"
+                                label="Your Height (cm)"
+                                className="mb-2"
+                            >
+                                <Form.Control type="text"
+                                              placeholder="180cm"
+                                              value={height}
+                                              onChange={(e) => setHeight(e.target.value)}
+                                />
+                            </FloatingLabel>
+                            <FloatingLabel controlId="floatingPassword" label="Your Weight (kg)">
+                                <Form.Control type="text"
+                                              placeholder="kg"
+                                              value={weight}
+                                              onChange={(e)=>setWeight(e.target.value)}
+
+                                />
+                            </FloatingLabel>
+                        </>
+
+                        <br/>
+
+                        <OverlayTrigger
+                            placement="right"
+                            delay={{show: 250, hide: 400}}
+                            overlay={renderTooltip}
+                        >
+
+                            <Button variant="success" className='button' onClick={calculateBMI}>BMI</Button>
+                        </OverlayTrigger>
+
+                        <hr/>
+                        {bmi && <h5>Your BMI: {bmi}</h5>}
+                    </Tab>
+
+
+                </Tabs>
+            </div>
+        </div>
+
+    </div>)
 }
